f49d7fa6d4fd8d16b7df866a4fc3020c34bc2908
Mete Sengul <mustafametesengul@gmail.com>
2026-01-24 17:40:19 +0300

    Add initial files (#1)

---
+++ DEVELOPMENT.md
@@ -0,0 +1,43 @@
+# Development
+
+Install dependencies:
+
+```bash
+uv sync
+```
+
+Sort imports:
+
+```bash
+uv run ruff check --select I --fix
+```
+
+Format code:
+
+```bash
+uv run ruff format
+```
+
+Run unit tests:
+
+```bash
+uv run pytest
+```
+
+Start services:
+
+```bash
+docker compose up
+```
+
+Run integration tests:
+
+```bash
+uv run pytest -m integration
+```
+
+Stop services:
+
+```bash
+docker compose down
+```
---
+++ tests/test_digestify.py
@@ -0,0 +1,25 @@
+import asyncio
+
+import pytest
+
+from digestify import Digestify, Topic
+
+
+@pytest.mark.integration
+async def test_digestify() -> None:
+    topic = Topic(
+        name="World News",
+        description=(
+            "A summary of the most important global news stories covering politics, "
+            "economics, conflicts, and major events from around the world."
+        ),
+    )
+
+    digestify = Digestify()
+
+    digest = await digestify.get_stories(topic)
+    print(digest.model_dump_json(indent=4))
+
+
+if __name__ == "__main__":
+    asyncio.run(test_digestify())
